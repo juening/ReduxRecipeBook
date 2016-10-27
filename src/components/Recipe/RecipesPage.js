f88a02cb09089ef9f1d759ae8b828e8097ea5dfa
Jue Ning <juening@gmail.com>
2016-10-26 23:22:23 -0400

    before redux kick in

--- src/components/Recipe/RecipesPage.js
+++ src/components/Recipe/RecipesPage.js
@@ -1,10 +1,31 @@
 import React, {Component, PropTypes} from 'react';
 
 class RecipesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      recipe: {title: ''}
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const recipe = this.state.recipe;
+    recipe.title = event.target.value;
+    this.setState({recipe: recipe});
+  }
+
+  onClickSave(){
+    alert(`Saving ${this.state.recipe.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Recipes</h1>
+        <input type="text" onChange={this.onTitleChange} value={this.state.recipe.title} />
+        <input type="submit" value="Save" onClick={this.onClickSave} />
       </div>
     );
   }
