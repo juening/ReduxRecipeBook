bd46ea858fceae8f5a61aa3177d1eeaa26d2121a
Jue Ning <juening@gmail.com>
2016-11-02 17:31:07 -0400

    production

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -6,6 +6,7 @@ class AboutPage extends Component {
       <div>
         <h1>About</h1>
         <p>This app uses React, React Router, Redux and a variety of other libraries. In the app you can create recipes, see an index view of the names of all recipes, and click into any of those recipes to view it. You can also edit or delete these recipes.</p>
+        <p>You may view the code <a href="https://github.com/juening/ReduxRecipeBook">here</a>.</p>
       </div>
     );
   }
